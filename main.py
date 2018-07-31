e0e7a1f8fb6c90093a7e70103f2474788f93416a
suusan2go <ksuzuki180@gmail.com>
2018-08-01 02:14:27 +0900

    load ldlibray in runtime

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+import ctypes
+
+for d, dirs, files in os.walk('lib'):
+    for f in files:
+        if f.endswith('.a'):
+            continue
+        ctypes.cdll.LoadLibrary(os.path.join(d, f))
+
 import pydicom
 import numpy as np
 import base64
