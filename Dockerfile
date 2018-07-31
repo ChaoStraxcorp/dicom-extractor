d8233e1bfbaa59ab7a9b672376fb6aab63f4c203
suusan2go <ksuzuki180@gmail.com>
2018-08-01 02:05:39 +0900

    fix deploy scripts

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM continuumio/miniconda3:4.5.4
 
+COPY . /app
+
 WORKDIR /app
   
-RUN conda install -c conda-forge gdcm && \
-  pip install -r requirements.txt -t ./dist && \
-  cp -r /opt/conda/lib/python3.6/site-packages/*gdcm* ./dist && \
-  mkdir -p ./dist/lib && \
-  cp -r /opt/conda/lib/*gdcm* ./dist/lib
+RUN conda install -c conda-forge gdcm
   
 RUN apt-get update && \
   apt-get install -y zip && \
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -eux
 
-cd ./dist
+# 依存関係を./distにインストール
+pip install -r requirements.txt -t ./dist
+# gdcm関連パッケージを./distにコピー
+cp -r /opt/conda/lib/python3.6/site-packages/*gdcm* ./dist
+mkdir -p ./dist/lib
+cp -r /opt/conda/lib/*gdcm* ./dist/lib
+
+# 容量削減のためtestsファイルを削除
+rm -rf ./dist/**/tests
+
+# メインファイルを./distにコピー
+cp ./main.py ./dist
+cp ./sample/explicit_ct_jpeg-lossless-nh_mono2.dcm ./dist
 zip -r deploy.zip .
 aws lambda update-function-code --function-name dicom-extractor --zip-file fileb:///app/dist/deploy.zip
