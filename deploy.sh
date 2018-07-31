9c6234456eedb6c63fa5379ce9860de8ce5740d6
suusan2go <ksuzuki180@gmail.com>
2018-08-01 02:23:19 +0900

    succes full deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -eux
 
+# 依存関係をクリア
+rm -rf ./dist/*
+
 # 依存関係を./distにインストール
 pip install -r requirements.txt -t ./dist
 # gdcm関連パッケージを./distにコピー
 cp -r /opt/conda/lib/python3.6/site-packages/*gdcm* ./dist
 mkdir -p ./dist/lib
-cp -r /opt/conda/lib/*gdcm* ./dist/lib
+cp -r /opt/conda/lib/libgdcm* ./dist/lib
+cp -r /opt/conda/lib/libsocketxx* ./dist/lib
 
 # 容量削減のためtestsファイルを削除
 rm -rf ./dist/**/tests
@@ -14,5 +18,6 @@ rm -rf ./dist/**/tests
 # メインファイルを./distにコピー
 cp ./main.py ./dist
 cp ./sample/explicit_ct_jpeg-lossless-nh_mono2.dcm ./dist
+cd ./dist
 zip -r deploy.zip .
 aws lambda update-function-code --function-name dicom-extractor --zip-file fileb:///app/dist/deploy.zip
